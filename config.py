c8a805b0c0778e8513ed17e98f734861ac5ac610
Paden B. Roder <roderpad@gmail.com>
2021-09-07 17:15:47 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,9 @@
+# #* Personal Paper Trading Alpaca API Key/Secret
+API_URL = 'https://paper-api.alpaca.markets'
+API_KEY = 'PKUGJ2P0WKLEE59T1YGN'
+API_SECRET = 'KjzCkszSNpYbzJa6Wts7dwoZnwO3e3v2zt4aVeDS'
+
+#DB_HOST = 'localhost'
+#DB_USER = 'postgres'
+#DB_PASS = 'fillin'
+#DB_NAME = 'fillin'
